8a5de7d931c791016a9b87ec509f5ff77337d75a
Allan Wazacz <cazwazacz@gmail.com>
2017-12-29 16:54:28 +0000

    Create middleware to check both query parameters are numbers, return 422 otherwise. Passes test.

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 var modulo = require('./lib/modulo');
 
+app.use(function(req, res, next) {
+  var a = parseInt(req.query.a);
+  var b = parseInt(req.query.b);
+  if (!a || !b || isNaN(a) || isNaN(b)) {
+    return res.status(422).end("You must specify two numbers as query parameters a and b")
+  }
+  return next();
+})
+
 app.get('/modulo', function(req, res) {
   res.json({ result: modulo(req.query.a, req.query.b) });
 })
