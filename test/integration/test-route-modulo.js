2e386816ba20999eabfbfa0514a16109dbc64071
Allan Wazacz <cazwazacz@gmail.com>
2017-12-29 16:45:47 +0000

    Add two failing tests

--- test/integration/test-route-modulo.js
+++ test/integration/test-route-modulo.js
@@ -1,4 +1,5 @@
 var supertest = require('supertest');
+var assert = require('assert');
 var app = require('../../index');
 
 describe('modulo', function() {
@@ -7,5 +8,23 @@ describe('modulo', function() {
       .get('/modulo?a=9&b=4')
       .expect(200)
       .end(done);
+  })
+
+  it('should reject strings', function(done) {
+      supertest(app)
+      .get('/modulo?a=9&b=string')
+      .expect(422)
+      .end(done);
+  })
+
+  it('should respond with a result that is a number', function(done) {
+      supertest(app)
+      .get('/modulo?a=9&b=4')
+      .expect(200)
+      .end(function(err, response) {
+        assert.ok(!err);
+        assert.ok(typeof response.body.result === 'number');
+        return done();
+      });
   })
 })
